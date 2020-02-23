eb7b69f0d3ba8aca40c9c6cae3a286eeddad3cc7
Muhammad Natsir <mnatsirm53@gmail.com>
2020-02-23 18:49:04 +0700

    Add login form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <React.Fragment>
         <NavBar />
         <Switch>
+          <Route path="/login" component={LoginForm}></Route>
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
