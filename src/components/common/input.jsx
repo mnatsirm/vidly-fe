1f378bd183add43190cd72846160dd45e13a8da9
Muhammad Natsir <mnatsirm53@gmail.com>
2020-02-23 23:37:13 +0700

    Extracting helper rendering methods

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 
 const Input = props => {
-  const { name, label, value, onChange, error } = props;
+  const { name, label, error, ...rest } = props;
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        name={name}
-        type="text"
-        className="form-control"
-      />
+      <input {...rest} id={name} name={name} className="form-control" />
       {error && <div className="aler alert-danger">{error}</div>}
     </div>
   );
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Input from "./common/input";
 import Form from "./common/form";
 import Joi from "joi-browser";
 
@@ -24,30 +23,15 @@ class LoginForm extends Form {
   };
 
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <Input
-              name="username"
-              value={data.username}
-              label="Username"
-              onChange={this.handleChange}
-              error={errors.username}
-            />
-            <Input
-              name="password"
-              value={data.password}
-              label="Password"
-              onChange={this.handleChange}
-              error={errors.password}
-            />
+            {this.renderInput("username", "Username")}
+            {this.renderInput("password", "Password", "password")}
           </div>
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+          {this.renderButton("Login")}
         </form>
       </div>
     );
