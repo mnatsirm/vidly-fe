432268d31a6435d15fec46c7e5e77b5277fb69c1
Muhammad Natsir <mnatsirm53@gmail.com>
2020-02-23 16:32:40 +0700

    Linking to MovieForm component

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const MovieForm = () => {
-  return <h1>movie form</h1>;
+const MovieForm = props => {
+  const { match, history } = props;
+  return (
+    <div>
+      <h1>Movie Form {match.params.id}</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push("/movies")}
+      >
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default MovieForm;
